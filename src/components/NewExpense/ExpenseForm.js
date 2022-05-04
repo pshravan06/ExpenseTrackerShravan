96e6283c6360e5f36fe27ad1daa78c5ea9df549b
pshravan06 <shravpar98@gmail.com>
2022-05-04 12:21:14 -0400

    added form code

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './ExpenseForm.css';
 
 const ExpenseForm = () => {
   return (
-    <div>ExpenseForm</div>
-  )
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2022-04-4" />
+        </div>
+      </div>
+      <div className='new-expense__actions'>
+          <button type="submit">ADD EXPENSE</button>
+      </div>
+    </form>
+  );
 }
 
 export default ExpenseForm
