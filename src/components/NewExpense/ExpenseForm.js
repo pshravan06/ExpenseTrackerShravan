f7ef4a77ab9e919339da9ba5cfbd38b72a907a8e
pshravan06 <shravpar98@gmail.com>
2022-05-04 20:52:34 -0400

    added two-way binding to form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -23,21 +23,24 @@ const ExpenseForm = () => {
           date: new Date(enteredDate)
         };
         console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input type="number" min="0.01" step="0.01"  value={enteredAmount}onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-04-4" onChange={dateChangeHandler} />
+          <input type="date" min="2019-01-01" max="2022-04-4" value={enteredDate}onChange={dateChangeHandler} />
         </div>
       </div>
       <div className='new-expense__actions'>
