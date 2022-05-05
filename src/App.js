31eebe45f59646a0ae6ebf1d5ca30e6f8c54a2db
pshravan06 <shravpar98@gmail.com>
2022-05-05 15:26:46 -0400

    passing data up to parent

--- src/App.js
+++ src/App.js
@@ -24,9 +24,14 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense)=>{
+    console.log('in app.js');
+    console.log(expense);
+  }
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense = {addExpenseHandler}/>
       <Expenses items={expenses}/>
     </div>
   );
