0acd874fe6f7a1b6baac903d083b1107d10ddbb0
pshravan06 <shravpar98@gmail.com>
2022-05-10 13:56:03 -0400

    formatting

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,21 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./ExpensesFilter.css";
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear]= useState('2019');
-  const filterChangeHandler = selectedYear =>{
-   setFilteredYear(selectedYear);
-  }
+  const [filteredYear, setFilteredYear] = useState("2019");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <div>
-
       <Card className="expenses">
-      <ExpensesFilter selected = {filteredYear}onChangeFilter={filterChangeHandler}/>
-
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
