49abe9b27fe2fcf0602c6fbfe0c7959b29ebba93
pshravan06 <shravpar98@gmail.com>
2022-05-12 13:54:08 -0400

    added conditional rendering using a ternary expression

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -20,15 +20,18 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {filteredExpenses.length === 0 ? (
+          <p>No Expenses Found.</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
       </Card>
     </div>
   );
