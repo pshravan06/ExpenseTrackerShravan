7aa97069ab09ff1e88fd3f4e02da154dada4681a
pshravan06 <shravpar98@gmail.com>
2022-05-12 12:56:16 -0400

    added key prop

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -19,12 +19,12 @@ const Expenses = (props) => {
 
         {props.items.map((expense) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
           />
         ))}
-        
       </Card>
     </div>
   );
